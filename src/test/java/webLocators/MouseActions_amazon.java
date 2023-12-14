602a6a4217b9dfadd9e636552b828ea604875ef2
abdulkhadarp <abdulkhadarpatan05@gmail.com>
2023-12-14 22:30:49 +0530

    committing changes in Maven project

--- src/test/java/webLocators/MouseActions_amazon.java
+++ src/test/java/webLocators/MouseActions_amazon.java
@@ -1,25 +1,34 @@
 package webLocators;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.interactions.Actions;
 
-
 public class MouseActions_amazon {
 
-	public static void main(String[] args) {
-		
-		System.setProperty("Webdriver.driver.chrome", "D:\\Automation Testing\\web drivers\\chromedriver.exe");
-		WebDriver driver= new ChromeDriver();
-		
-		driver.get("https://www.amazon.in");
-		Thread.sleep(5000);
-		
-		Actions mouseact= new Actions(driver);
-		
-		
-	    )
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+			
+			System.setProperty("Webdriver.driver.chrome", "D:\\Automation Testing\\web drivers\\chromedriver.exe");
+			WebDriver driver= new ChromeDriver();
+			
+			driver.get("https://www.amazon.in");
+			
+			driver.manage().window().maximize();
+			
+			Actions mouseact= new Actions(driver);
+			
+			WebElement about= driver.findElement(By.linkText("Interest-Based Ads"));
+			mouseact.scrollToElement(about).build().perform();
+			Thread.sleep(3000);
+			
+			driver.findElement(By.partialLinkText("-Based Ads")).click();
+			Thread.sleep(3000);
 
+			WebElement security = driver.findElement(By.linkText("Your Security"));
+			mouseact.moveToElement(security).build().perform();
 	}
 
 }
